d870b5f7fe60f72c1cccbccf49cee750428d38d3
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-11-29 10:36:57 +0900

    Fix to work

--- execs.py
+++ execs.py
@@ -3,28 +3,25 @@ import time
 import numpy as np
 
 from monitors import Monitor1
-from panels import ConsolePanel, IMDataPanel, PrinterPanel
+from panels import *
 
 
 def main1():
-    manager = mp.Manager()
-    pp = PrinterPanel(
-        manager,
+    pp = PrinterPanelV1(
         cap_target=0,
         md_labels={'title': 'sensor01', 'x': 'time', 'y': 'value'},
         md_legends=['current', 'target'],
         pd_labels={'title': 'sensor02'},
         pd_ranges={'x': (0, 100), 'y': (0, 100)},
         histlen=15)
-    cp = ConsolePanel(manager)
-    ip = IMDataPanel(
-        manager,
+    cp = ConsolePanelV1()
+    ip = IMDataPanelV1(
         d1_labels={'title': 'capture01', 'x': 'length', 'y': 'height [µm]'},
         d2_labels={'title': 'capture02', 'x': 'length', 'y': 'width [µm]'},
         d3_labels={'title': 'summary', 'x': 'length', 'y': 'width [µm]'},
         histlen=15)
 
-    mon = Monitor1('Test Monitor', pp, cp, ip)
+    mon = Monitor1('Ultimaker Live Monitor', pp, cp, ip)
     mp.Process(target=mon.run).start()
 
     for i in range(1000):
