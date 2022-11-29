6c5486c9b15642677c718fe4e98eeecae7d0233a
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-11-29 10:35:56 +0900

    Remove Manager

--- monitors.py
+++ monitors.py
@@ -17,24 +17,24 @@ class Monitor1:
         self.panel2 = panel2
         self.panel3 = panel3
 
-    def run(self, manager=None):
+    def run(self):
         app = pg.mkQApp()
         window = QtWidgets.QMainWindow()
         window.setWindowTitle(self.title)
 
         frame = pg.LayoutWidget()
+        frame.layout.setContentsMargins(5, 5, 5, 5)
         window.setCentralWidget(frame)
 
-        manager = mp.Manager()
         if self.panel1 is not None:
             frame.addWidget(
-                self.panel1.build_widget(manager), col=0, row=0, rowspan=2)
+                self.panel1.build_widget(), col=0, row=0, rowspan=2)
         if self.panel2 is not None:
             frame.addWidget(
-                self.panel2.build_widget(manager), col=0, row=2)
+                self.panel2.build_widget(), col=0, row=2)
         if self.panel3 is not None:
             frame.addWidget(
-                self.panel3.build_widget(manager), col=1, row=0, rowspan=3)
+                self.panel3.build_widget(), col=1, row=0, rowspan=3)
         frame.layout.setColumnStretch(0, 2)
         frame.layout.setColumnStretch(1, 3)
         window.show()
